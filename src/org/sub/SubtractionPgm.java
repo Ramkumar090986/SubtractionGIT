356b447db3361b3c4a8c9222676a2e0c55ede172
ramkumar090986 <ramkumar8687@gmail.com>
2023-12-14 15:35:04 +0530

    Sub Program

---
+++ src/org/sub/SubtractionPgm.java
@@ -0,0 +1,15 @@
+package org.sub;
+
+public class SubtractionPgm {
+	
+	public static void main(String[] args) {
+		
+		int a=10,b=5,res=0;
+		
+		res=a-b;
+		
+		System.out.println("Subtraction of two numbers:"+res);
+		
+	}
+
+}
